c27aaa970dd21a4aea0e0fc7127f987a9a51f77d
Seunghee Lee <seunghee127@naver.com>
2022-06-25 14:54:53 +0900

    검색 기능 개발

--- src/main/java/com/joy/MOVIERESERVATIONSYSTEM/repository/MovieRepository.java
+++ src/main/java/com/joy/MOVIERESERVATIONSYSTEM/repository/MovieRepository.java
@@ -4,10 +4,18 @@ import com.joy.MOVIERESERVATIONSYSTEM.domain.member.Member;
 import com.joy.MOVIERESERVATIONSYSTEM.domain.movie.Movie;
 import com.joy.MOVIERESERVATIONSYSTEM.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("select m from Movie m where m.region = :region")
+    List<Movie> findByRegion(@Param("region") Enum region);
+
+    @Query("select m from Movie m where m.title = :title")
+    List<Movie> findByTitle(@Param("title") String title);
 
 }
